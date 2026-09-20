2772009c1ca3c980cec2ea125c4c90a2d4965f29
woya187 <83906644+woya187@users.noreply.github.com>
2026-09-20 19:35:23 +0800

    修复价格显示重复$

--- monitor.py
+++ monitor.py
@@ -532,7 +532,7 @@ def generate_html_report(gainers, data, amplitude_top=None):
             <div class="amp-row">
                 <span class="amp-rank">{i}</span>
                 <span class="amp-name">{sym}</span>
-                <span class="amp-price">${format_price(price)}</span>
+                <span class="amp-price">{format_price(price)}</span>
                 <span class="amp-val">{amp:.2f}%</span>
             </div>"""
         amplitude_html = f"""
